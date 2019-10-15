a6af672543f8b09ca47a10769189d4888a4ed4b3
chudnofskyATD <mpereannor@gmail.com>
2019-10-15 14:57:03 +0000

    end of day breakpoint

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,14 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route, NavLink, withRouter, Redirect } from 'react-router-dom';
+import Login from './login';
+
 import './App.css';
 
 function App() {
+  const onLogout = () => {
+
+    localeStorage.clear();
+
+    history.replaceState('/')
+
+  }
   return (
     <div className="App">
-      <
+      <nav>
+        <NavLink exact to='/'> Login</NavLink>
+        <NavLink to='/friendslist'>Friends List</NavLink>
       
+      <button onClick={onLogout}>Logout</button>
+      </nav>
+
+      <main>
+        <Route exact path='/' component={Login}/>
+
+        <Route exact path='/friendslist' render={props => withAthCheck(friendslist, props)}
+        />
+        
+      </main>
+      />
     </div>
   );
 }
 
-export default App;
+function withAthCheck(Component, props) {
+  if (localeStorage.getItem('token')) {
+    return <Component {...props}/> 
+  }
+  return <Redirect to='/'/>
+}
+
+export default withRouter(App);
