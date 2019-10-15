b0cc2763bcd7b92dcffbd552f0ebd20e1eda1790
chudnofskyATD <mpereannor@gmail.com>
2019-10-15 12:40:29 +0000

    login component created

---
+++ friends/src/login.js
@@ -0,0 +1,13 @@
+function Login () { 
+
+    return (
+        <form onSubmit={}>
+            <h3>Log in</h3>
+            <input type="text" ref="username" placeholder="enter you username" />
+            <input type="password" ref="password" placeholder="enter password" />
+            <input type="submit" value="Login" />
+        </form>
+          
+    )
+}
+
